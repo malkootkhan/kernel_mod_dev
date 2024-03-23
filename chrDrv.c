d2a81639964cfb75008ffe756a578a7bafda8867
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-03-23 16:52:58 +0000

    updates: two character devices are created and 	register with kernel
    Signed-off-by: Malkoot Khan <engr.mkhan1990@gmail.com>

--- chrDrv.c
+++ chrDrv.c
@@ -1,16 +1,27 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/cdev.h>
-#include <linux/fs.h>
+#include <linux/module.h>   // Required for all kernel modules
+#include <linux/init.h>     // Needed for the macros module_init and module_exit
+#include <linux/fs.h>       // For the character device operations
+#include <linux/cdev.h>     // For registering and unregistering your device
+#include <linux/types.h>    // For standard data types used in the kernel
+#include <linux/uaccess.h>  // For transferring data between user and kernel space
+#include <linux/kdev_t.h>   // For device numbers
+#include <linux/device.h>   // For device creation in the sysfs
+#include <linux/kernel.h>   // For printk and other kernel operations
+#include <linux/slab.h>     // For memory allocation (if required)
+#include <linux/ioctl.h>    // For ioctl commands (if required)
+
 
 #define MINORBITS		(20)					/*20 lower bits of 32 bits device number*/
 #define MINORMASK		( (1U << MINORBITS) - 1)		/*shifting to left with 20 bits and subtracting by one will give us 20bits number eg: 111111...111*/
 #define MAJOR(dev)		((unsigned int)(dev >> MINORBITS))      /*shifting 20bits towards right will give us 12 significant bits which is major*/
 #define MINOR(dev)		((unsigned int)(dev & MINORMASK))	/*it will give the lower 20 bits which is minor number*/
-#define DEVICE_NAME		("my_test_device")
-#define CLASS_NAME		("my_test_class")
+#define MKDEV(ma, mi)		((unsigned int)((ma << MINORBITS) | mi ))
+#define DEVICE_NAME		"my_test_device_"
+#define CLASS_NAME		"my_test_class"
 #define DEFAULT_MINOR		(0)
-#define DEV_COUNT		(1)
+#define DEV_COUNT		(2)
+#define GET_DEV_NAME(x)		DEVICE_NAME "%d",x
+#define DEBUG
 
 typedef enum 
 {
@@ -21,8 +32,8 @@ typedef enum
 
 int test_point = 0;
 struct class *class;
-struct cdev dev_cdev;
-static dev_t dev_num;
+struct cdev dev_cdev[DEV_COUNT];
+dev_t dev_num;
 struct file_operations fop;
 
 /*main function that is called by kernel*/
@@ -30,8 +41,11 @@ struct file_operations fop;
 static int __init chr_drv_init(void)
 {
 	int ret;
+	int index;
+	dev_t curr_dev;
+	
 
-	printk("test_point 1: Welcome to character drv\n");
+	printk("test_point %d: Welcome to character drv\n", ++test_point);
 
 	/*Step 01: Create class in the first step so that we can link the device this class you can see class in /sys/class/ and within class you can see your device what you linked*/
 	class = class_create(CLASS_NAME);
@@ -40,37 +54,61 @@ static int __init chr_drv_init(void)
 	ret = alloc_chrdev_region(&dev_num, DEFAULT_MINOR, DEV_COUNT, DEVICE_NAME);
 	if(ret < 0)
 	{
-		printk("The device number couldn't be allocated\n");
+		printk("test_point %d: device number couldn't be allocated\n", ++test_point);
 		return FAILURE; 
 	}
-	
-	/*Step 03: initialize device here*/
-	cdev_init(&dev_cdev, &fop);
+	for(index = 0; index < DEV_COUNT; index++)
+	{
 
-	/*Step 04: add the device to system*/
-	cdev_add(&dev_cdev, dev_num, DEV_COUNT);
+		/*Step 03: initialize device here*/
+#ifdef DEBUG
+		printk("test_point %d.%d: cdev_init function is following\n", ++test_point,index);
+#endif
+		cdev_init(&dev_cdev[index], &fop);
 
-	/*Step 05: it will create a special file located in /dev direcory*/
-	device_create(class, NULL, dev_num, NULL, DEVICE_NAME);
+		curr_dev = MKDEV( MAJOR(dev_num), (MINOR(dev_num)+index) );
+
+		/*Step 04: add the device to system*/
+#ifdef DEBUG
+		printk("test_point %d.%d: cdev_add function is following\n", ++test_point,index);
+#endif
+		cdev_add(&dev_cdev[index], curr_dev, DEV_COUNT);
+
+		/*Step 05: it will create a special file located in /dev direcory*/
+#ifdef DEBUG
+		printk("test_point %d.%d: device_create function is following\n", ++test_point,index);
+#endif
+		device_create(class, NULL, curr_dev, NULL, GET_DEV_NAME(index));
+	}
 	return SUCCESS;
 
 }
 
 static void __exit chr_drv_exit(void)
 {
-	printk("test_point %d: Good bye", test_point);
-	/*Step 01: device destroy means removing the device special file /dev*/
-	device_destroy(class, dev_num);
+	int index;
+	dev_t curr_dev;
 
-	/*Step 02: cdev_del makes the system forget about the character device*/
-	cdev_del(&dev_cdev);
+	printk("test_point %d: Good bye", ++test_point);
 
+	for(index = 0; index < DEV_COUNT; index++)
+	{
+
+		curr_dev = MKDEV ( MAJOR(dev_num), (MINOR(dev_num)+index) );
+
+		/*Step 01: device destroy means removing the device special file /dev*/
+		device_destroy(class, curr_dev);
+
+		/*Step 02: cdev_del makes the system forget about the character device*/
+		cdev_del(&dev_cdev[index]);
+
+		/*Step 04: this function will release the device number*/
+		unregister_chrdev_region(curr_dev, DEV_COUNT);
+	}
+	
 	/*Step 03: class_destroy and class_unregister will deregister and remove the class from the system*/
 	class_unregister(class);
 	class_destroy(class);
-
-	/*Step 04: this function will release the device number*/
-	unregister_chrdev_region(dev_num, DEV_COUNT);
 }
 
 
